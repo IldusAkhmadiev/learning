a75eaa4cc046effae22ea2a4ce3c76d034539804
Ildus Akhmadiev <anan308an@gmail.com>
2024-11-26 09:57:55 +0300

    Добавить слайды

--- src/main/java/com/github/ildus_akhmadiev/learning/rest/LanguageRestController.java
+++ src/main/java/com/github/ildus_akhmadiev/learning/rest/LanguageRestController.java
@@ -1,6 +1,7 @@
 package com.github.ildus_akhmadiev.learning.rest;
 
 import com.github.ildus_akhmadiev.learning.dto.AnswerDTO;
+import com.github.ildus_akhmadiev.learning.dto.PronounAnswerDTO;
 import com.github.ildus_akhmadiev.learning.service.PracticeService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,18 +20,26 @@ public class LanguageRestController {
         this.practiceService = practiceService;
     }
 
-    @PostMapping("/{lessonId}/submit")
-    public ResponseEntity<String> submitAnswer(
-            @PathVariable String lessonId,
-            @RequestBody AnswerDTO answerDTO
+    @PostMapping("/submit")
+    public ResponseEntity<Map<String, Object>> submitAnswer(
+            @RequestBody PronounAnswerDTO answerDTO
     ) {
         // Проверяем правильность ответа
-        boolean isCorrect = practiceService.checkPronounTranslation(answerDTO.getAnswer());
+        boolean isCorrect = practiceService.checkPronounTranslation(
+                answerDTO.getPronoun(),
+                answerDTO.getAnswer()
+        );
 
         // Получаем обратную связь
-        String feedback = practiceService.getAnswerFeedback(isCorrect);
+        String feedback = practiceService.getAnswerFeedback(
+                answerDTO.getPronoun(),
+                isCorrect
+        );
 
         // Возвращаем ответ
-        return ResponseEntity.ok(feedback);
+        return ResponseEntity.ok(Map.of(
+                "correct", isCorrect,
+                "feedback", feedback
+        ));
     }
 }
--- src/main/java/com/github/ildus_akhmadiev/learning/service/PracticeService.java
+++ src/main/java/com/github/ildus_akhmadiev/learning/service/PracticeService.java
@@ -6,15 +6,26 @@ import java.util.Map;
 
 @Service
 public class PracticeService {
-    public boolean checkPronounTranslation(String answer) {
-        // Правильный перевод местоимения "I" - "я"
-        return "я".equals(answer);
+    // Словарь правильных ответов для каждого местоимения
+    private static final Map<String, String> CORRECT_TRANSLATIONS = new HashMap<>() {{
+        put("I", "я");
+        put("You", "ты");
+        put("He/She/It", "он");
+    }};
+
+    public boolean checkPronounTranslation(String pronoun, String answer) {
+        // Проверяем правильность перевода
+        return CORRECT_TRANSLATIONS.get(pronoun).equals(answer);
     }
 
-    public String getAnswerFeedback(boolean isCorrect) {
-        return isCorrect
-                ? "Верно! 'I' переводится как 'я'."
-                : "Неверно. 'I' переводится как 'я'.";
+    public String getAnswerFeedback(String pronoun, boolean isCorrect) {
+        // Формируем обратную связь
+        if (isCorrect) {
+            return String.format("Верно! '%s' переводится как '%s'.",
+                    pronoun, CORRECT_TRANSLATIONS.get(pronoun));
+        } else {
+            return String.format("Неверно. '%s' переводится как '%s'.",
+                    pronoun, CORRECT_TRANSLATIONS.get(pronoun));
+        }
     }
-
 }
